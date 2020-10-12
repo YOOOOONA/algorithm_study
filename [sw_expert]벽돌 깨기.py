f35d4d4afee37c6efb14370c568080dad61bb33d
김윤아 <42761910+YOOOOONA@users.noreply.github.com>
2020-10-13 02:12:39 +0900

    20.10.13

--- [sw_expert]벽돌 깨기.py
+++ [sw_expert]벽돌 깨기.py
@@ -4,7 +4,7 @@ Created on Sun Oct 11 20:09:47 2020
 
 @author: 융
 """
-'''
+
 import sys
 sys.stdin = open('sample_input.txt','r')
 input = sys.stdin.readline
@@ -32,14 +32,34 @@ def crushBlock(N,W,H,mat):
             if r == 0: continue
             else:
                 li = i-(r-1) if i-(r-1) > 0 else 0; ri = i+(r-1) if i+(r-1) < W-1 else W-1 
-                mat[li:ri][j] = 0
+                #mat[li:ri][j] = 0
+                for k in range(li,ri+1):
+                    print('범위',li,ri)
+                    mat[k][j] = 0
                 lj = j-(r-1) if j-(r-1) > 0 else 0; rj = j+(r-1) if j+(r-1) < H-1 else H-1
-                mat[i][lj:rj] = 0
-                print('i와 mat',i,'\n',mat,'\n')
+                for k in range(lj,rj+1):
+                    mat[i][k] = 0
+                #print('j와 mat',j,'\n',mat,'\n')
                 break
+        #지우고 나서 중간에 빈칸 있으면 없애줘야돼
+        for c in range(W):
+            flag = 0
+            upper = []
+            for r in range(H):
+                blc = mat[r][c]
+                if blc != 0:
+                    if flag == 0: flag = 1; 
+                    upper.append(blc)
+                elif blc == 0 and flag == 1: flag = 0
+            #쌓은 upper를 다시 빈칸없이 mat 의 column에 셋팅하기
+            upLen = len(upper)
+            for r in range(upLen):
+                mat[r][c] = upper[upLen - 1 - r]
+                    
         ##################################
-        crushBlock(N-1,W,H,mat)
-    print(mat,'\n')
+        #crushBlock(N-1,W,H,mat)
+        
+    #print(mat,'\n')
 
 
 if __name__ == '__main__':
@@ -53,11 +73,14 @@ if __name__ == '__main__':
         mat = [list(map(int, input().split())) for _ in range(H)]
         blockCnts = []
         crushBlock(N,W,H,mat)
+        print(blockCnts)
         print("#{} {}".format(tc+1, max(blockCnts)))
+        break
 '''
 a = [[0,0,0,1],[2,1,3,0]]
 for i in a:
     print(i.count(0))
 for i in range(2):
-    a[i][2] =999
+    a[i][2] =999#넘파이아니라서 a[1:3][0] = 99 이런거 안됨
 print(a)
+'''
