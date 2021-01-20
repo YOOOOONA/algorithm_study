0caf002f518445687e73be940c4891e526d05bb7
yoon <yoonakim0808@gmail.com>
2021-01-21 01:37:26 +0900

    21.01.21

--- [프로그래머스]불량사용자.py
+++ [프로그래머스]불량사용자.py
@@ -13,9 +13,12 @@ def isMatch(user_set, banned_set):
     
 def solution(user_id, banned_id):
     ans=[]
-    for com_set in permutations(user_id, len(banned_id)):
+    for com_set in permutations(user_id, len(banned_id)):#이걸 set으로 중복검사를 하는게 더 오래걸림.
+        
         if isMatch(com_set, banned_id):
-            com_set = set(com_set) # 중복 제거
+            print(1,com_set,type(com_set))#tuple
+            com_set = {*com_set} # 중복 제거아니고,이 형태(set)로 가야되는거
+            print(2,com_set,type(com_set))#set
             if com_set not in ans:
                 ans.append(com_set)
     return len(ans)
