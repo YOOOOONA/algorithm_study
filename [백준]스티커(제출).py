982058fca7fac8e6271b6ebdb70757b2a65c7e08
yoon <yoonakim0808@gmail.com>
2021-02-02 02:00:35 +0900

    21.02.02

--- [백준]스티커(제출).py
+++ [백준]스티커(제출).py
@@ -1,6 +1,7 @@
 #스티커 점수 합 최대가 되게 스티커 떼기
 # 뗄수있는 스티커 점수의 최댓값
 # 한번떼면 상하좌우 다 못씀
+#recursion too mucg,, 점화식으로 풀어야됨. dfs가 아니라
 import sys
 import copy
 sys.stdin = open('.\\input.txt')#output:260,290
@@ -8,41 +9,30 @@ input = sys.stdin.readline
 from collections import deque
 st=[]
 n=0
-def dfs(res,i,j,visit): 
+def dfs(res,i,j): 
     global ansLi
     #n==1일때 해야됨
     if res==i==j==0:
-        dfs(res,1,0,visit)
+        dfs(res,1,0)
     if j ==0:
-        visit = [[0 for _ in range(n)] for _ in range(2)]
-        visit[i][j]=1
         res+=st[i][j]
         i=(i+1)%2
         j+=1
     if j == n-1:
-        visit_temp = copy.deepcopy(visit)
-        visit_temp[i][j] = 1
-        # print(res+st[i][j])
         ansLi.append(res+st[i][j])
         return
     elif j > n-1:
-        # print(res)
         ansLi.append(res)
         return
     else:
-        visit_temp = copy.deepcopy(visit)
-        visit_temp[i][j] = visit_temp[(i+1)%2][j+1] = 1
-        dfs(res + st[i][j] + st[(i+1)%2][j+1] , i,j+2,visit_temp)
-        visit_temp = copy.deepcopy(visit)            
-        visit_temp[i][j+1] = 1
-        dfs(res + st[i][j+1] , (i+1)%2,j+2,visit_temp)
+        dfs(res + st[i][j] + st[(i+1)%2][j+1] , i,j+2)
+        dfs(res + st[i][j+1] , (i+1)%2,j+2)
         
 T = int(input())
 for _ in range(T):
     n = int(input())
     ansLi = []
     st = [list(map(int,input().split())) for _ in range(2)]
-    visit = [[0 for _ in range(n)] for _ in range(2)]
     res=0
-    dfs(res,0,0,visit)
+    dfs(res,0,0)
     print(max(ansLi))
---
+++ [프로그래머스]메뉴리뉴얼.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+import sys
+sys.stdin = open('.\\input.txt')
+input = sys.stdin.readline
+
+# for i in combinations("ABC",2):
+#     print(i)
+
+def solution(orders,course):
+    return ans
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5])
+print(solution(["XYZ", "XWY", "WXA"],[2,3,4])
+print(solution(["ABD", "ABC", "ABCD"],[3])
+"""
+output:
+["AC", "ACDE", "BCFG", "CDE"]
+["ACD", "AD", "ADE", "CD", "XYZ"]
+["WX", "XY"]
+["ABC", "ABD"]
+"""
